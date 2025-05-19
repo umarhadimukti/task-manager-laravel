bce8bb93fd44dc171a2aea691d26ce92dd672749
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-05-19 22:31:21 +0700

    create api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ActivityLogController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/**
+ * public route
+ */
+Route::post('/login', [AuthController::class, 'login']);
+
+/**
+ * protected route
+ */
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Authentication
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Users
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    
+    // Tasks
+    Route::apiResource('tasks', TaskController::class);
+    
+    // Logs
+    Route::get('/logs', [ActivityLogController::class, 'index']);
+});
