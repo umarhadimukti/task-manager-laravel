7caf3feee2bd348fcbc0127a4bff671aec35f6b1
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-05-20 09:21:58 +0700

    register command and seeder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Console\Commands\CheckOverdueTasks;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -21,6 +22,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'checkUserStatus' => \App\Http\Middleware\CheckUserStatus::class,
         ]);
     })
+    ->withCommands([
+        CheckOverdueTasks::class,
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
